aedc04a55f4e8a244188b5e9977b28e034b24641
rik <xxd0225@gmail.com>
2016-11-22 23:31:36 +0800

    update 005

--- 005/add005.py
+++ 005/add005.py
@@ -1,11 +1,13 @@
-import re, sys
+import os
 
-def staticsWords(fileName):
-    pattern = r'[a-zA-Z0-9]+'
-    with open(fileName) as f:
-        wordsCount = re.findall(pattern, f.read())
-    return len(wordsCount)
+from PIL import Image
 
-if __name__=='__main__':
-    filePath = sys.path[0] + '/word.txt'
-    print staticsWords(filePath)
+Image
+
+def processPicture(resource, dest, picName, imageType):
+    imageType = 'jpep' if imageType == 'jpg' else 'png'
+    im = Image.open(resource + picName)
+    rate = max(im.size[0]/640.0 if im.size[0] > 640 else 0, im.size[1]/1136.0 if im.size[1] > 1136 else 0)
+    if rate:
+        im.thumbnail((im.size[0] / rate, im.size[1] / rate))
+    im.save(dest + picName, imageType)
